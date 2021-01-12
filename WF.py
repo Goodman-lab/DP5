1f984d3163eec2588feb5bffba3e9c99944e006a
HowarthA <ah809@cam.ac.uk>
2021-01-12 11:27:09 +0000

    finished integration and GUI support - adding test scripts now

--- WF.py
+++ WF.py
@@ -131,7 +131,7 @@ def kde_probs(Isomers,wfData,sigma):
         wfData.UnscaledAtomProbs[iso] = [[] for i in wfData.AtomReps[iso]]
         wfData.ScaledAtomProbs[iso] = [[] for i in wfData.AtomReps[iso]]
 
-        maxproc = 10
+        maxproc = 5
 
         pool = mp.Pool(maxproc)
 
